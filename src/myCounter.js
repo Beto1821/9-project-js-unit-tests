5f1bf6ae45fd210c4e0fee8316290989246c9df5
Beto1821 <beto1821@uol.com.br>
2022-04-06 22:07:00 -0300

    req08 rev01

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,12 +20,11 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let i = 2; i <= 3; i += 1) {
+      myArray.push(i);
     }
   }
   return myArray;
 };
-
 
 module.exports = myCounter;
