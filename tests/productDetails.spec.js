44ca850d4ebc134b5314adab8a28b0199dbe8346
Beto1821 <beto1821@uol.com.br>
2022-04-06 21:12:15 -0300

    req06 rev00

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,14 +27,34 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+  // it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+  //   fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    test('Teste se productDetails é uma função', () => {
+      expect(typeof productDetails).toBe('function')
+    });
     // Teste se o retorno da função é um array.
+    test('Teste se o retorno da função é um array', () => {
+        expect(Array.isArray(productDetails()));
+    });
     // Teste se o array retornado pela função contém dois itens dentro.
+    test('Teste se o array retornado pela função contém dois itens dentro', () => {
+      expect(Object.keys(productDetails()).length).toEqual(2);
+    });
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    test('Teste se os dois itens dentro do array retornado pela função são objetos', () => {
+      expect(typeof productDetails()).toEqual('object');
+    });
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    test('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si', () => {
+      const product = productDetails('123' , '456');
+      expect(product[0].name[0]).not.toEqual(product[1].name[1]);
+    });
     // Teste se os dois productIds terminam com 123.
-  });
+    test('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si', () => {
+      const product = productDetails('aaa123' , 'aaa123');
+      expect(product[0].name[0].substr(-3)).toEqual(product[1].name[1].substr(-3));
+    });
+  // });
 });
